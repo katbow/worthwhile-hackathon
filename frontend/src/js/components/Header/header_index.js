bc5f3a84613a4f4c6d3336620e9515718402a70d
shouston3 <shouston3@me.com>
2016-04-29 14:34:07 +0100

    added an image to the header

--- frontend/src/js/components/Header/header_index.js
+++ frontend/src/js/components/Header/header_index.js
@@ -17,7 +17,8 @@ export default class Header extends React.Component {
           expanded={this.state.menuOpen}
           onToggle={() => { this.setState({ menuOpen: !this.state.menuOpen }) }}
           className='top-menu'
-          fixedTop={true}>
+          fixedTop={true}
+          style={{backgroundImage: 'url("https://cloud.githubusercontent.com/assets/15983736/14917508/4a5f9d10-0e17-11e6-8354-ab7e7520cc88.JPG"'}}>
           <Navbar.Header>
             <Navbar.Brand>
               <Link to={'/'}>
