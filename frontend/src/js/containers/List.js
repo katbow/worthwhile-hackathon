26a695a0a21e4cb8ce91d09ac908787a8adb855a
shouston3 <shouston3@me.com>
2016-04-29 09:06:01 +0100

    set out an initial list down the page

--- frontend/src/js/containers/List.js
+++ frontend/src/js/containers/List.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class List extends Component {
+const styles = {
+  li: {
+    listStyleType: 'none',
+    color: 'white',
+    fontSize: '1.7em',
+    textDecoration: 'none',
+    padding: '5px',
+    margin: '9px',
+    backgroundColor: 'green',
+    borderRadius: '10px'
+  },
+  ul: {
+    padding: '2em',
+    borderRadius: '10px'
+  }
+} 
+
+class List extends Component {
   render() {
+    console.log(this.props.eventList[0])
     return(
-      <div>
-        <h1>List</h1>
-      </div>
+      <ul style={styles.ul}>
+        {this.props.eventList.map(event => {
+          return (
+            <li style={styles.li}>
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <p><span>Venue: </span>{event.venue}</p>
+              <div>{event.attendees}</div>
+            </li>
+          )}
+        )} 
+      </ul>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    eventList: state.eventList
+  }
+}
+
+export default connect(mapStateToProps)(List) 
